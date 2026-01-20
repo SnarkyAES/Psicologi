4da0436fc0b2d06c0059d8e48feba911a89cefaf
SnarkyAES <snarkyaes@proton.me>
2026-01-20 22:52:16 +0100

    Update main_v3.py

--- main_v3.py
+++ main_v3.py
@@ -23,9 +23,9 @@ st.caption(
 # INPUT
 # =========================
 R_priv = st.slider("Incassi PRIVATI annui (includono 4% integrativo) €", 0, 85000, 24000, 500)
-R_pa   = st.slider("Incassi PA annui (includono 2% integrativo) €", 0, 85000, 12000, 500)
+R_pa   = st.slider("Incassi PA annui (includono 2% integrativo) €", 0, 85000, 36000, 500)
 
-spese_studio = st.slider("Spese fisse di studio annue €", 0, 50000, 0, 500)
+spese_studio = st.slider("Spese fisse di studio annue €", 0, 50000, 12000, 500)
 soggettivo_pct = st.slider("% contributo soggettivo ENPAP", 10, 30, 24, 1)
 
 st.divider()
